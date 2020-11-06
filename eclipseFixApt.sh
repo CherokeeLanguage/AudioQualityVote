5580f0867004ef4e9b8864b8b068e5999ecca17b
Michael Conrad <m.conrad.202@gmail.com>
2020-11-06 12:54:00 -0500

    multi project setup fix for eclipse import

---
+++ eclipseFixApt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+ 
+cd "$(dirname "$0")"
+cwd="$(pwd)"
+gradle="$cwd/gradlew"
+
+for x in . *; do
+	cd "$cwd"
+	if [ ! -d "$x" ]; then continue; fi
+	if [ ! -f "$x"/build.gradle ]; then echo "*** skip $x"; continue; fi
+	cd "$x"
+	echo "=== $x"
+	rm .settings/org.eclipse.jdt.apt.core.prefs 2> /dev/null || true
+	rm .settings/org.eclipse.jdt.core.prefs 2> /dev/null || true
+	rm .factorypath 2> /dev/null || true
+	$gradle eclipseJdtApt > /dev/null
+	$gradle eclipseFactorypath > /dev/null
+done
+	
