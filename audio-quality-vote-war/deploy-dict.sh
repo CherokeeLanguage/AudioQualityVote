be04b4d16837cffd306078fc2e551456dbe7e409
Michael Conrad <m.conrad.202@gmail.com>
2021-06-05 19:50:07 -0400

    Deploy CED specific instance

---
+++ audio-quality-vote-war/deploy-dict.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+trap "echo ERROR" ERR
+
+gradle clean build war
+
+scp build/libs/AudioQualityVote.war clcom@vhost.cherokeelessons.com:/var/lib/tomcat9/webapps/AudioQualityVote.tmp
+
+ssh clcom@vhost.cherokeelessons.com 'cd /var/lib/tomcat9/webapps && mv -v AudioQualityVote.tmp CED-AudioQualityVote.war'
+
